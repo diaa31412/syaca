dc6d8d64f41b07ace3f44062da38d0891c774ce6
diaa31412 <dhariri31412@gmail.com>
2025-08-23 03:34:20 +0300

    update my code in indexModelr

--- models/indexModel.js
+++ models/indexModel.js
@@ -1,15 +1,15 @@
-
-const User = require('./userModel');
-const Course = require('./courseModel');
-const Subscriber = require('./subscriberModel');
-const Video = require('./videoModel');
-const Card = require('./cardModel'); 
-const UserProgress = require('./userProgressModel');
-const CourseProgress = require('./courseProgressModel');
-const Session = require('./sessionModel');
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
-
+// Import models
+const User = require('./userModel')(sequelize, DataTypes);
+const Course = require('./courseModel')(sequelize, DataTypes);
+const Subscriber = require('./subscriberModel')(sequelize, DataTypes);
+const Video = require('./videoModel')(sequelize, DataTypes);
+const Card = require('./cardModel')(sequelize, DataTypes);
+const UserProgress = require('./userProgressModel')(sequelize, DataTypes);
+const CourseProgress = require('./courseProgressModel')(sequelize, DataTypes);
+const Session = require('./sessionModel')(sequelize, DataTypes);
 
 // User-Course many-to-many through Subscriber
 User.belongsToMany(Course, {
@@ -17,113 +17,45 @@ User.belongsToMany(Course, {
   foreignKey: 'userId',
   otherKey: 'courseId',
   as: 'subscribedCourses',
-  onDelete: 'CASCADE' // Optional: cascade delete
+  onDelete: 'CASCADE',
 });
-
 Course.belongsToMany(User, {
   through: Subscriber,
   foreignKey: 'courseId',
   otherKey: 'userId',
   as: 'subscribers',
-  onDelete: 'CASCADE' // Optional: cascade delete
+  onDelete: 'CASCADE',
 });
 
-// Subscriber belongs to both User and Course
-Subscriber.belongsTo(User, { 
-  foreignKey: 'userId',
-  as: 'user'
-});
-
-Subscriber.belongsTo(Course, { 
-  foreignKey: 'courseId',
-  as: 'course' 
-});
-
-// Add association to Card if subscriptions use cards
-Subscriber.belongsTo(Card, {
-  foreignKey: 'cardId',
-  as: 'card'
-});
+// Subscriber associations
+Subscriber.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+Subscriber.belongsTo(Course, { foreignKey: 'courseId', as: 'course' });
+Subscriber.belongsTo(Card, { foreignKey: 'cardId', as: 'card' });
 
 // Course-Video one-to-many
-Course.hasMany(Video, { 
-  foreignKey: 'courseId',
-  as: 'videos',
-  onDelete: 'CASCADE' // Typically videos are deleted when course is deleted
-});
+Course.hasMany(Video, { foreignKey: 'courseId', as: 'videos', onDelete: 'CASCADE' });
+Video.belongsTo(Course, { foreignKey: 'courseId', as: 'course' });
 
-Video.belongsTo(Course, { 
-  foreignKey: 'courseId',
-  as: 'course'
-});
+// Session association
+Session.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
+// Card associations
+User.hasMany(Card, { foreignKey: 'userId', as: 'cards' });
+Card.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
-// seesion model 
-Session.associate = models => {
-  Session.belongsTo(models.User, { foreignKey: 'userId' });
-};
+// UserProgress associations
+UserProgress.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+UserProgress.belongsTo(Course, { foreignKey: 'courseId', as: 'course' });
+UserProgress.belongsTo(Video, { foreignKey: 'videoId', as: 'video' });
+User.hasMany(UserProgress, { foreignKey: 'userId', as: 'progress' });
+Course.hasMany(UserProgress, { foreignKey: 'courseId', as: 'progress' });
+Video.hasMany(UserProgress, { foreignKey: 'videoId', as: 'progress' });
 
-
-// If cards belong to users
-User.hasMany(Card, {
-  foreignKey: 'userId',
-  as: 'cards'
-});
-
-Card.belongsTo(User, {
-  foreignKey: 'userId',
-  as: 'user'
-});
-
-UserProgress.belongsTo(User,
-   { foreignKey: 'userId', 
-    as: 'user' 
-});
-
-UserProgress.belongsTo(Course, 
-  { foreignKey: 'courseId',
-     as: 'course' 
-});
-
-UserProgress.belongsTo(Video, 
-  { foreignKey: 'videoId',
-   as: 'video' 
-});
-
-User.hasMany(UserProgress,
-   { foreignKey: 'userId', 
-  as: 'progress' 
-});
-
-Course.hasMany(UserProgress, 
-  { foreignKey: 'courseId',
-     as: 'progress'
- });
-
-Video.hasMany(UserProgress, 
-  { foreignKey:'videoId',
-     as: 'progress' 
-});
-
-CourseProgress.belongsTo(User, 
-  { foreignKey: 'userId', 
-as: 'user' });
-
-
-CourseProgress.belongsTo(Course,
-   { foreignKey: 'courseId', 
-as: 'course'
-});
-
-User.hasMany(CourseProgress,
-   { foreignKey: 'userId', 
-  as: 'courseProgress'
- });
- 
-Course.hasMany(CourseProgress, 
-  { foreignKey: 'courseId', 
-    as: 'courseProgress' 
-});
+// CourseProgress associations
+CourseProgress.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+CourseProgress.belongsTo(Course, { foreignKey: 'courseId', as: 'course' });
+User.hasMany(CourseProgress, { foreignKey: 'userId', as: 'courseProgress' });
+Course.hasMany(CourseProgress, { foreignKey: 'courseId', as: 'courseProgress' });
 
 module.exports = {
   sequelize,
@@ -131,5 +63,8 @@ module.exports = {
   Course,
   Subscriber,
   Video,
-  Card
+  Card,
+  UserProgress,
+  CourseProgress,
+  Session,
 };
